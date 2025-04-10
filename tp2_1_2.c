696fbeb4d19195ace91f5eab6645c73abfc3cba0
Facundet <facundotrejo2021@gmail.com>
2025-04-10 03:04:54 -0300

    cambio a notacion indexada

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 #define N 20
-// codigo a completar
+// debo usar notacion indexada
 
 int main(void)
 {
@@ -13,8 +13,8 @@ int main(void)
     double vt[N];
     for (i = 0; i < N; i++)
     {
-        vt[i] = 1 + rand() % 100;
-        printf("% f   ", vt[i]);
+        *(vt + 1) = 1 + rand() % 100;
+        printf("% f   ", *(vt + 1));
     }
 
     return 0;
